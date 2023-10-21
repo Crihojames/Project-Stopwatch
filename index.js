7f07b8c455737c020b0618acc37eaa09c60552de
Crihojames <crihojames@gmail.com>
2023-10-21 17:48:25 +0100

    finished

--- index.js
+++ index.js
@@ -14,19 +14,39 @@ function startTimer(){
             elapsedTime = Date.now() - startTime;
             timerEl.textContent = formatTime(elapsedTime);
         }, 10)
-}
+
+        startButtonEl.disabled = true;
+        stopButtonEl.disabled = false;
+
+    }
 
 function formatTime(elapsedTime){
-    const miliseconds = Math.floor((elapsedTime % 1000) / 10);
-    return miliseconds;
+    const milliseconds = Math.floor((elapsedTime % 1000) / 10);
+    const seconds = Math.floor((elapsedTime % (1000 * 60)) / (1000));
+    const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+    const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    return (
+        (hours ?(hours > 9 ? hours : "0" + hours) : "00")
+         +":"+
+        (minutes ?(minutes > 9 ? minutes : "0" + minutes) : "00")
+         +":"+
+        (seconds ?(seconds > 9 ? seconds : "0" + seconds) : "00")
+         +"."+
+        (milliseconds > 9 ? milliseconds : "0" + milliseconds));
 }
 
 function stopTimer(){
-    console.log("stop");
+    clearInterval(timerInterval);
+    startButtonEl.disabled = false;
+    stopButtonEl.disabled = true;
 }
 
 function resetTimer(){
-    console.log("reset");
+    clearInterval(timerInterval);
+    elapsedTime = 0;
+    timerEl.textContent = formatTime(elapsedTime);
+    startButtonEl.disabled = false;
+    stopButtonEl.disabled = true;
 }
 
 
